60265ac7a14c8491b18632493b02a8cbd4a9dc52
Judah <judahben149@gmail.com>
2023-08-03 22:15:26 +0100

    Setup binding for fragments

--- app/src/main/java/com/judahben149/motiontraction/presentation/movieList/MovieListFragment.kt
+++ app/src/main/java/com/judahben149/motiontraction/presentation/movieList/MovieListFragment.kt
@@ -1,21 +1,36 @@
 package com.judahben149.motiontraction.presentation.movieList
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.judahben149.motiontraction.R
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.judahben149.motiontraction.BuildConfig
+import com.judahben149.motiontraction.databinding.FragmentMovieListBinding
 
 
 class MovieListFragment : Fragment() {
+
+    private var _binding: FragmentMovieListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_list, container, false)
+        _binding = FragmentMovieListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Snackbar.make(binding.root, BuildConfig.API_KEY, Snackbar.LENGTH_INDEFINITE).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
