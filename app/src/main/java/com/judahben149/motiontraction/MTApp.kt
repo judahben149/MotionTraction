2678bc9e4333c04b086918e78ca8653e12913b64
Judah <judahben149@gmail.com>
2023-08-03 20:16:50 +0100

    Set up Hilt for Dependency Injection

---
+++ app/src/main/java/com/judahben149/motiontraction/MTApp.kt
@@ -0,0 +1,8 @@
+package com.judahben149.motiontraction
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MTApp: Application() {
+}
--- app/src/main/java/com/judahben149/motiontraction/presentation/MainActivity.kt
+++ app/src/main/java/com/judahben149/motiontraction/presentation/MainActivity.kt
@@ -8,23 +8,28 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
 import com.judahben149.motiontraction.R
 import com.judahben149.motiontraction.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
