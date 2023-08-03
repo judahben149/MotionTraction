bcce27bd1e3bfe759ae18fde1235a794151a28c7
Judah <judahben149@gmail.com>
2023-08-03 23:43:24 +0100

    Added app constants

---
+++ app/src/main/java/com/judahben149/motiontraction/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.judahben149.motiontraction.utils
+
+object Constants  {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/"
+    const val NETWORK_PAGE_SIZE = 20
+    const val STARTING_PAGE_INDEX = 1
+
+    const val MOVIE_DATABASE = "movie_database"
+}
