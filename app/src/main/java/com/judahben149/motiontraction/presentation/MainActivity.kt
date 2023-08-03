c95bde07ff3d0101f3657999931e829e74d9ccc5
Judah <judahben149@gmail.com>
2023-08-03 19:40:39 +0100

    Refactored packages and setup navigation

--- app/src/main/java/com/judahben149/motiontraction/presentation/MainActivity.kt
+++ app/src/main/java/com/judahben149/motiontraction/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.judahben149.motiontraction
+package com.judahben149.motiontraction.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +8,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.judahben149.motiontraction.R
 import com.judahben149.motiontraction.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
