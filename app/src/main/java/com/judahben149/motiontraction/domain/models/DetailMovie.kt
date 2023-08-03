abcc8ef98ecce3f00d389ceadfbb879bb53c6923
Judah <judahben149@gmail.com>
2023-08-03 23:55:41 +0100

    Added movie domain models

---
+++ app/src/main/java/com/judahben149/motiontraction/domain/models/DetailMovie.kt
@@ -0,0 +1,27 @@
+package com.judahben149.motiontraction.domain.models
+
+data class DetailMovie(
+
+    val adult: Boolean = false,
+    val backdropPath: String = "",
+    val budget: Double = 0.0,
+//    val genres: List<GenreX>,
+    val homepageUrl: String = "",
+    val id: Int = 0,
+    val imdbId: String = "",
+    val originalLanguage: String = "",
+    val originalTitle: String = "",
+    val overview: String = "",
+    val popularity: Double = 0.0,
+    val posterPath: String = "",
+    val releaseDate: String = "",
+    val revenue: Double = 0.0,
+    val runtime: Int = 0,
+//    val spokenLanguages: List<SpokenLanguage>,
+    val status: String = "",
+    val tagline: String = "",
+    val title: String = "",
+    val video: Boolean = false,
+    val voteAverage: Double = 0.0,
+    val voteCount: Int = 0
+)
---
+++ app/src/main/java/com/judahben149/motiontraction/domain/models/ListMovie.kt
@@ -0,0 +1,9 @@
+package com.judahben149.motiontraction.domain.models
+
+data class ListMovie(
+    val id: Int = 0,
+    val backdropPath: String = "",
+    val title: String = "",
+    val releaseDate: String = "",
+    val posterPath: String = ""
+)
