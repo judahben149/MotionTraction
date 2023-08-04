63bea7c4b448818c19086183001b1fb8016240dd
Judah <judahben149@gmail.com>
2023-08-04 03:38:47 +0100

    implemented functionality to fetch movie list and update paging adapter

--- app/src/main/java/com/judahben149/motiontraction/presentation/movieList/MovieListFragment.kt
+++ app/src/main/java/com/judahben149/motiontraction/presentation/movieList/MovieListFragment.kt
@@ -5,28 +5,72 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.material.snackbar.Snackbar
-import com.judahben149.motiontraction.BuildConfig
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.judahben149.motiontraction.R
 import com.judahben149.motiontraction.databinding.FragmentMovieListBinding
+import com.judahben149.motiontraction.databinding.ItemLoadMoreBinding
+import com.judahben149.motiontraction.presentation.movieList.adapter.MovieListAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class MovieListFragment : Fragment() {
 
     private var _binding: FragmentMovieListBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var adapter: MovieListAdapter
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var footerBinding: ItemLoadMoreBinding
+    private val viewModel: MovieListViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMovieListBinding.inflate(inflater, container, false)
+        footerBinding = ItemLoadMoreBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews()
+        collectState()
+    }
 
-        Snackbar.make(binding.root, BuildConfig.API_KEY, Snackbar.LENGTH_INDEFINITE).show()
+    private fun initViews() {
+        recyclerView = binding.rvDiscoverMovies
+
+        adapter = MovieListAdapter(requireContext()) { id ->
+            val bundle = Bundle()
+            bundle.putInt("MOVIE_ID", id)
+            findNavController().navigate(R.id.MovieDetailFragment, bundle)
+        }
+
+        val layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = layoutManager
+    }
+
+    private fun collectState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.movieListCached.collectLatest {
+                    adapter.submitData(pagingData = it)
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/judahben149/motiontraction/presentation/movieList/MovieListViewModel.kt
+++ app/src/main/java/com/judahben149/motiontraction/presentation/movieList/MovieListViewModel.kt
@@ -1,11 +1,23 @@
 package com.judahben149.motiontraction.presentation.movieList
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.judahben149.motiontraction.data.repository.MovieRepositoryImpl
+import com.judahben149.motiontraction.domain.mappers.entityToMovieListModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
-class MovieListViewModel @Inject constructor(): ViewModel() {
+class MovieListViewModel @Inject constructor(repository: MovieRepositoryImpl): ViewModel() {
 
+    private val _movieListCached = repository.fetchDiscoverMovieListCached().map { pagingData ->
+        pagingData.map {  movieEntity ->
+            entityToMovieListModel(movieEntity)
+        }
+    }.cachedIn(viewModelScope)
 
+    val movieListCached get() = _movieListCached
 }
