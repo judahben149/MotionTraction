b3326cd5cdb7420ce5b71bf6a0023960f5790d2e
Judah <judahben149@gmail.com>
2023-08-06 04:59:51 +0100

    Updated utils file

--- app/src/main/java/com/judahben149/motiontraction/utils/DateUtils.kt
+++ app/src/main/java/com/judahben149/motiontraction/utils/DateUtils.kt
@@ -1,13 +1,52 @@
 package com.judahben149.motiontraction.utils
 
+import com.judahben149.motiontraction.domain.models.Genre
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 fun String.parseFriendlyDate(): String {
 
-    val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-    val date = inputFormat.parse(this)
+    return if (this.isEmpty()) {
+        "N/A"
+    } else {
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val date = inputFormat.parse(this)
 
-    val outputFormat = SimpleDateFormat("dd MMM, yyyy", Locale.getDefault())
-    return outputFormat.format(date!!)
+        val outputFormat = SimpleDateFormat("dd MMM, yyyy", Locale.getDefault())
+        outputFormat.format(date!!)
+    }
+}
+
+fun Int.parseRunTime(): String {
+    var hours = 0
+    var minutes = 0
+
+    return if (this >= 60) {
+        hours = this / 60
+        minutes = this % 60
+        "${hours.toString()}h ${minutes.toString()}m"
+    }
+    else {
+        minutes = this
+        "${minutes.toString()}m"
+    }
+}
+
+fun List<Genre>.parseGenres(): String {
+    if (this.isEmpty()) {
+        return "N/A"
+    } else {
+        var genres = ""
+
+        for (i in this.indices) {
+            genres += this[i].name
+
+            if (i != this.size - 1) {
+                genres += "/"
+            }
+        }
+
+//        genres.dropLast(3)
+        return genres
+    }
 }
