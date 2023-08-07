ba0ad5ee897aec1c2b760be433b2ba10dae89146
Judah <judahben149@gmail.com>
2023-08-07 02:12:23 +0100

    Updated util files for text and image operations

---
+++ app/src/main/java/com/judahben149/motiontraction/utils/ImageUtils.kt
@@ -0,0 +1,45 @@
+package com.judahben149.motiontraction.utils
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.judahben149.motiontraction.utils.Constants.BACKDROP_BASE_URL
+
+fun ImageView.loadImage(
+    context: Context,
+    url: String,
+    onResourceReady:() -> Unit
+) {
+
+    Glide.with(context)
+        .load(BACKDROP_BASE_URL + url)
+        .listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                onResourceReady()
+                return false
+            }
+        })
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .into(this)
+}
