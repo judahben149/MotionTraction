22a04399da3be062f201e691548c0b19e64a564c
Judah <judahben149@gmail.com>
2023-08-07 23:52:46 +0100

    Added functionality to save single movies

--- app/src/main/java/com/judahben149/motiontraction/data/source/local/MovieDao.kt
+++ app/src/main/java/com/judahben149/motiontraction/data/source/local/MovieDao.kt
@@ -5,13 +5,21 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.judahben149.motiontraction.data.source.local.entity.MovieDetailEntity
 import com.judahben149.motiontraction.data.source.local.entity.MovieResponseEntity
+import io.reactivex.Observable
 
 @Dao
 interface MovieDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertMovies(movieEntity: List<MovieResponseEntity.MovieEntity>)
+    fun saveAllMovies(movieEntityList: List<MovieResponseEntity.MovieEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveMovie(movieEntity: MovieDetailEntity)
+
+    @Query("SELECT * FROM movie WHERE id = :movieId")
+    fun getMovie(movieId: Long): Observable<MovieDetailEntity>
 
     @Query("SELECT * FROM movies ORDER BY id ASC")
     fun getAllMovies(): PagingSource<Int, MovieResponseEntity.MovieEntity>
--- app/src/main/java/com/judahben149/motiontraction/data/source/local/MovieDatabase.kt
+++ app/src/main/java/com/judahben149/motiontraction/data/source/local/MovieDatabase.kt
@@ -2,10 +2,11 @@ package com.judahben149.motiontraction.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.judahben149.motiontraction.data.source.local.entity.MovieDetailEntity
 import com.judahben149.motiontraction.data.source.local.entity.MovieEntityRemoteKey
 import com.judahben149.motiontraction.data.source.local.entity.MovieResponseEntity
 
-@Database(entities = [MovieResponseEntity.MovieEntity::class, MovieEntityRemoteKey::class], version = 1, exportSchema = false)
+@Database(entities = [MovieResponseEntity.MovieEntity::class, MovieEntityRemoteKey::class, MovieDetailEntity::class], version = 1, exportSchema = false)
 abstract class MovieDatabase: RoomDatabase() {
     abstract val movieDao: MovieDao
     abstract val remoteKeyDao: RemoteKeyDao
