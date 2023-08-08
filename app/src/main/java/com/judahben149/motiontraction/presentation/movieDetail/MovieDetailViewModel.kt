9098736c101f83025c645a15f29ce02eb5c201a2
Judah <judahben149@gmail.com>
2023-08-08 04:11:55 +0100

    Refactored code to use merge operator to make 2 resource requests in one go

--- app/src/main/java/com/judahben149/motiontraction/presentation/movieDetail/MovieDetailViewModel.kt
+++ app/src/main/java/com/judahben149/motiontraction/presentation/movieDetail/MovieDetailViewModel.kt
@@ -4,11 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.judahben149.motiontraction.data.repository.MovieRepositoryImpl
+import com.judahben149.motiontraction.data.source.local.entity.credits.CreditsEntity
+import com.judahben149.motiontraction.data.source.local.entity.movieDetail.MovieDetailEntity
 import com.judahben149.motiontraction.domain.mappers.toCredits
 import com.judahben149.motiontraction.domain.mappers.toDetailMovie
-import com.judahben149.motiontraction.domain.models.DetailMovie
-import com.judahben149.motiontraction.domain.models.credits.Credits
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -23,95 +24,68 @@ class MovieDetailViewModel @Inject constructor(private val repository: MovieRepo
     val state: LiveData<MovieDetailUiState> = _state
 
     private lateinit var disposable: Disposable
-    private lateinit var creditsDisposable: Disposable
 
-    fun getMovieDetail(movieId: Int) {
-        repository.getMovieDetail(movieId)
-            .map { it.toDetailMovie() }
-            .subscribeOn(Schedulers.io())
+
+    fun getMovie(movieId: Int) {
+        clearError()
+
+        Observable.merge(
+            repository.getMovieDetail(movieId).subscribeOn(Schedulers.io()),
+            repository.getMovieCredits(movieId).subscribeOn(Schedulers.io())
+        ).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(getMovieDetailObserver())
+            .subscribe(getMovieObserver())
     }
 
-    fun getMovieCredits(movieId: Int) {
-        repository.getMovieCredits(movieId)
-            .map { it.toCredits() }
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(getMovieCreditsObserver())
-    }
-
-    private fun getMovieDetailObserver(): Observer<DetailMovie> {
-        return object : Observer<DetailMovie> {
+    private fun getMovieObserver() : Observer<Any> {
+        return object : Observer<Any> {
             override fun onSubscribe(d: Disposable) {
-                // show progress bar
                 disposable = d
                 toggleLoadingState(true)
             }
 
             override fun onError(e: Throwable) {
                 e.printStackTrace()
-                _state.value = _state.value?.copy(
-                    isError = true,
-                    errorMessage = e.message.toString(),
-                )
-            }
-
-            override fun onComplete() {}
-
-            override fun onNext(movie: DetailMovie) {
-                _state.value = _state.value?.copy(
-                    movieDetail = movie,
-                    isGetMovieDetailSuccessful = true
-                )
-
-                toggleLoadingState(!(_state.value?.isGetMovieDetailSuccessful == true && _state.value?.isGetMovieCastSuccessful == true))
-            }
-        }
-    }
-
-    private fun getMovieCreditsObserver(): Observer<Credits> {
-        return object : Observer<Credits> {
-            override fun onSubscribe(d: Disposable) {
-                // show progress bar
-                creditsDisposable = d
-                toggleLoadingState(true)
-            }
-
-            override fun onError(e: Throwable) {
-                e.printStackTrace()
-                _state.value = _state.value?.copy(
-                    isError = true,
-                    errorMessage = e.message.toString(),
-                    isLoading = false
-                )
+                postError(e.message.toString())
             }
 
             override fun onComplete() {
-                toggleLoadingState(isLoading = false)
+                toggleLoadingState(false)
             }
 
-            override fun onNext(credits: Credits) {
-//                if (_state.value?.isGetMovieDetailSuccessful == true) {
+            override fun onNext(item: Any) {
+                if (item is MovieDetailEntity) {
                     _state.value = _state.value?.copy(
-                        credits = credits,
+                        movieDetail = item.toDetailMovie(),
+                        isGetMovieDetailSuccessful = true
+                    )
+                    toggleLoadingState(!(_state.value?.isGetMovieDetailSuccessful == true && _state.value?.isGetMovieCastSuccessful == true))
+                }
+                else if (item is CreditsEntity) {
+                    _state.value = _state.value?.copy(
+                        credits = item.toCredits(),
                         isGetMovieCastSuccessful = true
                     )
-//                }
-
-                toggleLoadingState(!(_state.value?.isGetMovieDetailSuccessful == true && _state.value?.isGetMovieCastSuccessful == true))
+                    toggleLoadingState(!(_state.value?.isGetMovieDetailSuccessful == true && _state.value?.isGetMovieCastSuccessful == true))
+                }
             }
         }
     }
-
 
     private fun toggleLoadingState(isLoading: Boolean) {
         _state.postValue(_state.value?.copy(isLoading = isLoading))
     }
 
+    private fun postError(errorMessage: String) {
+        _state.postValue(_state.value?.copy(isError = true, errorMessage = errorMessage))
+    }
+
+    private fun clearError() {
+        _state.postValue(_state.value?.copy(isError = false))
+    }
+
     override fun onCleared() {
         super.onCleared()
         disposable.dispose()
-        creditsDisposable.dispose()
     }
 }
