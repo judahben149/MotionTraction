ca92ba85e4863074b2d8c50bcb491ee8d054ed2d
Judah <judahben149@gmail.com>
2023-08-09 13:48:22 +0100

    Setup shimmer and swipe refresh functionality in Movie Detail Screen

--- app/src/main/java/com/judahben149/motiontraction/presentation/movieDetail/MovieDetailFragment.kt
+++ app/src/main/java/com/judahben149/motiontraction/presentation/movieDetail/MovieDetailFragment.kt
@@ -7,9 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
 import com.judahben149.motiontraction.databinding.FragmentMovieDetailBinding
 import com.judahben149.motiontraction.presentation.movieDetail.epoxy.MovieDetailEpoxyController
+import com.judahben149.motiontraction.utils.showSnackBar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,10 +20,15 @@ class MovieDetailFragment : Fragment() {
     private var movieId: Int = 0
 
     private val viewModel: MovieDetailViewModel by viewModels()
-    private val controller by lazy {
-        MovieDetailEpoxyController(requireContext()) { handleLikeAction() }
-    }
     private val navController by lazy { findNavController() }
+
+    private val controller by lazy {
+        MovieDetailEpoxyController(
+            context = requireContext(),
+            onMovieLiked = { handleLikeAction() },
+            onLoadedItemsComplete = { toggleShimmer(false) })
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +43,32 @@ class MovieDetailFragment : Fragment() {
         binding.btnBack.setOnClickListener { navController.popBackStack() }
         binding.epoxyRvMovieDetail.setController(controller)
 
+        setupListeners()
         observeState()
-        makeRequest()
+        getMovieDetail()
+    }
+
+    private fun setupListeners() {
+        binding.swipeRefresh.setOnRefreshListener {
+            getMovieDetail()
+            binding.swipeRefresh.isRefreshing = false
+        }
     }
 
     private fun observeState() {
         viewModel.state.observe(viewLifecycleOwner) { state ->
             if (state.isGetMovieDetailSuccessful && state.isGetMovieCastSuccessful) {
+//                toggleShimmer(false)
                 controller.setData(state)
             }
 
             if (state.isError) {
-                Snackbar.make(binding.root, state.errorMessage, Snackbar.LENGTH_LONG).show()
+                state.errorMessage.showSnackBar(binding.root)
             }
         }
     }
 
-    private fun makeRequest() {
+    private fun getMovieDetail() {
         val id = arguments?.getInt("MOVIE_ID")
         id?.let { movieId = it }
 
@@ -63,6 +77,26 @@ class MovieDetailFragment : Fragment() {
 
     private fun handleLikeAction() {
         viewModel.toggleIsFavorite()
+    }
+
+    private fun toggleShimmer(shouldStart: Boolean) {
+        if (shouldStart) {
+            binding.shimmerLayoutDetail.shimmer.visibility = View.VISIBLE
+            binding.shimmerLayoutDetail.shimmer.startShimmer()
+        } else {
+            binding.shimmerLayoutDetail.shimmer.visibility = View.INVISIBLE
+            binding.shimmerLayoutDetail.shimmer.stopShimmer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        toggleShimmer(true)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        toggleShimmer(false)
     }
 
     override fun onDestroy() {
