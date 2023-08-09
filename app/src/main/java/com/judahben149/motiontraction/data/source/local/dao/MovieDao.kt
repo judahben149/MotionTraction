0ca6e9158a19d7c459d8170f73dd99ab1f78727e
Judah <judahben149@gmail.com>
2023-08-09 02:48:49 +0100

    Updated refactoring changes in dao and database

--- app/src/main/java/com/judahben149/motiontraction/data/source/local/dao/MovieDao.kt
+++ app/src/main/java/com/judahben149/motiontraction/data/source/local/dao/MovieDao.kt
@@ -5,9 +5,12 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.judahben149.motiontraction.data.source.local.entity.movieDetail.MovieDetailEntity
+import com.judahben149.motiontraction.data.source.local.entity.movieDetail.MovieDetailEntityUpdate
 import com.judahben149.motiontraction.data.source.local.entity.movieList.MovieResponseEntity
 import io.reactivex.Observable
+import io.reactivex.Single
 
 @Dao
 interface MovieDao {
@@ -18,8 +21,20 @@ interface MovieDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun saveMovie(movieEntity: MovieDetailEntity)
 
-    @Query("SELECT * FROM movie WHERE id = :movieId")
+    @Query("SELECT * FROM movie WHERE movieId = :movieId")
     fun getMovie(movieId: Long): Observable<MovieDetailEntity>
+
+    @Query("SELECT * FROM movie WHERE movieId = :movieId")
+    fun peekMovie(movieId: Long): Single<MovieDetailEntity?>
+
+    @Query("UPDATE movies SET isFavorite = :isFavorite WHERE movieId = :movieId")
+    fun updateFavoriteMovieListItem(movieId: Long, isFavorite: Boolean): Single<Int>
+
+    @Query("UPDATE movie SET isFavorite = :isFavorite WHERE movieId = :movieId")
+    fun updateFavoriteMovieDetailItem(movieId: Int, isFavorite: Boolean): Single<Int>
+
+    @Update(entity = MovieDetailEntity::class)
+    fun updateMovieDetail(movieDetailUpdate: MovieDetailEntityUpdate): Single<Int>
 
     @Query("SELECT * FROM movies ORDER BY id ASC")
     fun getAllMovies(): PagingSource<Int, MovieResponseEntity.MovieEntity>
