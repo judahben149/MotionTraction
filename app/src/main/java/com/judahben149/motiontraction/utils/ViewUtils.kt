ebd5a81dd7c464a295d5f428bfeb01c890318f20
Judah <judahben149@gmail.com>
2023-08-09 22:43:56 +0100

    Setup view utils

---
+++ app/src/main/java/com/judahben149/motiontraction/utils/ViewUtils.kt
@@ -0,0 +1,33 @@
+package com.judahben149.motiontraction.utils
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.facebook.shimmer.ShimmerFrameLayout
+
+fun SwipeRefreshLayout.hide() {
+    this.visibility = View.GONE
+}
+
+fun RecyclerView.hide() {
+    this.visibility = View.GONE
+}
+
+fun ShimmerFrameLayout.hide() {
+    this.visibility = View.GONE
+}
+
+fun SwipeRefreshLayout.show() {
+    this.visibility = View.GONE
+}
+
+fun RecyclerView.show() {
+    this.visibility = View.GONE
+}
+
+fun ShimmerFrameLayout.show() {
+    this.visibility = View.GONE
+}
+
+
+
