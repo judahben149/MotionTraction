5d766a4b422fda8dc4269bbcfbb179a04925f21f
Judah <judahben149@gmail.com>
2023-08-11 00:07:00 +0100

    Updated text and view utils

--- app/src/main/java/com/judahben149/motiontraction/utils/TextUtils.kt
+++ app/src/main/java/com/judahben149/motiontraction/utils/TextUtils.kt
@@ -1,8 +1,12 @@
 package com.judahben149.motiontraction.utils
 
+import com.judahben149.motiontraction.domain.models.credits.Crew
 import com.judahben149.motiontraction.domain.models.movieDetail.Genre
+import com.judahben149.motiontraction.domain.models.movieDetail.ProductionCompany
 import java.text.SimpleDateFormat
 import java.util.Locale
+import kotlin.math.pow
+import kotlin.math.roundToInt
 
 fun String.parseFriendlyDate(): String {
 
@@ -33,8 +37,8 @@ fun Int.parseRunTime(): String {
 }
 
 fun List<Genre>.parseGenres(): String {
-    if (this.isEmpty()) {
-        return "N/A"
+    return if (this.isEmpty()) {
+        "N/A"
     } else {
         var genres = ""
 
@@ -46,7 +50,80 @@ fun List<Genre>.parseGenres(): String {
             }
         }
 
-//        genres.dropLast(3)
-        return genres
+        genres
     }
+}
+
+fun Double.parseVoteAverage(numberVoted: Double): String {
+    val trimmedRating = String.format("%.1f", this)
+    return "${trimmedRating}/10 (${numberVoted.roundToInt()})"
+}
+
+fun List<Crew>.returnDirectorsAndWriters(): String {
+    val directorList = this.filter { it.job == "Director" }
+    val writerList = this.filter { it.job == "Writer" }
+
+    var directors = ""
+    var writers = ""
+
+    for (i in directorList.indices) {
+        directors += this[i].name
+
+        if (i != directorList.size - 1) {
+            directors += ", "
+        }
+
+        if (i == directorList.size - 1){
+            directors = "Directors: $directors \n"
+        }
+    }
+
+    for (i in writerList.indices) {
+        writers += this[i].name
+
+        if (i != writerList.size - 1) {
+            writers += ", "
+        }
+
+        if (i == writerList.size - 1){
+            writers = "Writers: $writers"
+        }
+    }
+
+    return directors + writers
+}
+
+fun List<ProductionCompany>.formatProductionCompanies(): String {
+    var companyList = ""
+
+    for (i in this.indices) {
+        if (i == 1) {
+            companyList += this[i].name
+        }
+
+        if (i > 1 && i < this.size -1) {
+            companyList += "  •  " + this[i].name + "(${ this[i].name })"
+        }
+
+        if (i != this.size - 1) {
+            companyList += ""
+        }
+    }
+
+    return companyList
+}
+
+fun Double.formatMoney(): String {
+    val suffixes = charArrayOf(' ', 'k', 'M', 'B', 'T', 'P', 'E')
+    var num = this.toLong()
+    var tier = 0
+
+    while (num >= 1000) {
+        num /= 1000
+        tier++
+    }
+
+    val formattedValue = String.format("%.1f", this / 1000.0.pow(tier.toDouble()))
+
+    return "$formattedValue${suffixes[tier]}"
 }
--- app/src/main/java/com/judahben149/motiontraction/utils/ViewUtils.kt
+++ app/src/main/java/com/judahben149/motiontraction/utils/ViewUtils.kt
@@ -1,6 +1,7 @@
 package com.judahben149.motiontraction.utils
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.facebook.shimmer.ShimmerFrameLayout
@@ -27,6 +28,14 @@ fun RecyclerView.show() {
 
 fun ShimmerFrameLayout.show() {
     this.visibility = View.GONE
+}
+
+fun TextView.hide() {
+    this.visibility = View.INVISIBLE
+}
+
+fun TextView.show() {
+    this.visibility = View.VISIBLE
 }
 
 
