6bc8667463cbd73c427df20de0cb8843ebfa3013
Judah <judahben149@gmail.com>
2023-08-10 01:57:26 +0100

    Refactored OperationResult sealed class Error case to take in data as Throwable as opposed to string previously. This is to aid testing

--- app/src/main/java/com/judahben149/motiontraction/utils/OperationResult.kt
+++ app/src/main/java/com/judahben149/motiontraction/utils/OperationResult.kt
@@ -2,5 +2,5 @@ package com.judahben149.motiontraction.utils
 
 sealed class OperationResult<out T> {
     data class Success<out T>(val data: T) : OperationResult<T>()
-    data class Error(val errorMessage: String) : OperationResult<Nothing>()
+    data class Error(val throwable: Throwable) : OperationResult<Nothing>()
 }
